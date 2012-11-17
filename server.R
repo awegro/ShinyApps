6c6f0f2328030c5ff8316d422ba8159a0df0a43f
Andrew Elliott <contact@awegroup.com>
2012-11-17 06:21:07 -0800

    Initial files that display Chase Corporate Challenge Results by company and gender
    Initial files that display Chase Corporate Challenge Results by company
    and gender.

---
+++ server.R
@@ -0,0 +1,56 @@
+library(shiny)
+library(stringr)
+library(ggplot2)
+
+Running = read.delim("~/ShinyApps/ChaseCorporateMF2012", header = TRUE, sep = "\t")
+
+Hours <- as.integer(((str_extract(str_extract(as.character(Running$Time),"^[0-9]:"),"[0-9]"))))
+Hours[is.na(Hours)] <- 0
+Minutes <- as.integer(str_extract(str_extract(as.character(Running$Time),"[0-9][0-9]:"),"[0-9][0-9]"))
+Seconds <- as.integer(str_extract(str_extract(as.character(Running$Time),":[0-9]*"),"[0-9][0-9]"))
+TotalTime <- Hours*3600+Minutes*60+Seconds
+
+Running <- cbind(Running,TotalTime)
+
+x <- Running[with(Running,order(TotalTime)), ]
+qplot(TotalTime/60,Name, data = x, color = Gender)
+
+x <- transform(x,Name=reorder(Name, TotalTime) ) 
+
+shinyServer(function(input, output) {
+  
+  # Compute the forumla text in a reactive function since it is 
+  # shared by the output$caption and output$mpgPlot functions
+  formulaText <- reactive(function() {
+    paste(input$variable," ",sep='')
+  })
+  
+  # Return the formula text for printing as a caption
+  output$caption <- reactiveText(function() {
+    formulaText()
+  })
+  
+  # Generate a plot of the requested variable against mpg and only 
+  # include outliers if requested
+  output$Runners <- reactivePlot(function() {
+#     if (checkboxInput$outliers == FALSE) {
+# #                                 #   p <- qplot(TotalTime/60,data = subset(subset(x,Company == paste(input$company)),Gender != paste(input$variable,'')),binwidth=1, fill=Gender)+ylab("Runner Count")+xlab("Minutes") + scale_y_continuous(breaks=seq(0, 10, 1))
+# #                                     p <- qplot(TotalTime/60,Name, data = subset(subset(x,Company == paste(input$company)),Gender != paste(input$variable,'')), color = Gender)+xlab("Minutes")+ylab("Runner")+ ggtitle("Histogram")
+# #                                    print(p)}
+#       print("YES")}
+#   else {
+    if (input$company == "ALL") {
+      p <- qplot(TotalTime/60,Name, data = subset(x,Gender != paste(input$variable,'')), color = Gender)+xlab("Minutes")+ylab("Runner")+ ggtitle(paste(input$company))
+      print(length(data))
+      print(p)
+    } else {
+      p <- qplot(TotalTime/60,Name, data = subset(subset(x,Company == paste(input$company)),Gender != paste(input$variable,'')), color = Gender)+xlab("Minutes")+ylab("Runner")+ ggtitle(paste(input$company))
+      print(p)
+      #print(str(checkboxInput$outliers))
+     }
+    #p <- qplot(TotalTime/60,data = Running[which((Running$Gender) != paste(input$variable,'')),],binwidth=1, fill=Gender)+ylab("Runner Count")+xlab("Minutes") + scale_y_continuous(breaks=seq(0, 10, 1))
+#     p <- qplot(TotalTime/60,Name, data = subset(subset(x,Company == paste(input$company)),Gender != paste(input$variable,'')), color = Gender)+xlab("Minutes")+ylab("Runner")+ ggtitle(paste(input$company))
+#     print(p)
+  }, height = 60+as.integer(12*nrow(subset(subset(x,Company == paste(input$company)),Gender != paste(input$variable,'')))))
+  
+})
---
+++ ui.R
@@ -0,0 +1,30 @@
+library(shiny)
+
+Running = read.delim("~/ShinyApps/ChaseCorporateMF2012", header = TRUE, sep = "\t")
+y = unique(Running$Company)
+y = y[sort.list(y)]
+#heightsetting  = 60+as.integer(12*nrow(subset(subset(x,Company == paste(input$company)),Gender != paste(input$variable,''))))
+
+# Define UI for miles per gallon application
+shinyUI(pageWithSidebar(
+  
+  # Application title
+  headerPanel("Chase Corporate Challenge Run 2012"),
+  
+  sidebarPanel(
+    selectInput("company", "Company:",
+                y),
+    
+    selectInput("variable", "Gender:",
+                list("All" = "Null",
+                     "Male" = "F", 
+                     "Female" = "M" 
+                     )),
+    
+    checkboxInput("outliers", "Show Histogram", FALSE)
+  ),
+  
+  mainPanel(
+    plotOutput("Runners")
+  )
+))
